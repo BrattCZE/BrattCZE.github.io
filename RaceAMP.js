3d4888a413e2a8f962c7eedc472b0d0c924f91b1
BrattCZE <104774374+BrattCZE@users.noreply.github.com>
2022-05-02 20:20:45 +0200

    Update RaceAMP.js

--- RaceAMP.js
+++ RaceAMP.js
@@ -53,7 +53,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualt
 
 		base_url : {//Where are your images at?
 			image : 'https://baranan.github.io/minno-tasks/images/ampImages'
-		}
+		},
 		sortingLabel1 : 'Příjemný', //Response is coded as 0. 
 		sortingLabel2 : 'Nepříjemný',  //Response is coded as 1.
 		randomizeLabelSides : false, //IF false, then label1 is on the left, and label2 is on the right.
