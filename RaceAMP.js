16746c5370aa903b6f645afb6fa027669b2bf855
BrattCZE <104774374+BrattCZE@users.noreply.github.com>
2022-05-03 14:05:07 +0200

    Update RaceAMP.js

--- RaceAMP.js
+++ RaceAMP.js
@@ -54,51 +54,51 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualt
 		base_url : {//Where are your images at?
 			image : 'https://baranan.github.io/minno-tasks/images/ampImages'
 		},
-		sortingLabel1 : 'Příjemný', //Response is coded as 0. 
-		sortingLabel2 : 'Nepříjemný',  //Response is coded as 1.
+		sortingLabel1 : 'příjemnější', //Response is coded as 0. 
+		sortingLabel2 : 'nepříjemnější',  //Response is coded as 1.
 		randomizeLabelSides : false, //IF false, then label1 is on the left, and label2 is on the right.
 		exampleBlockInst: '<div><p style="font-size:20px; text-align:left; margin-left:10px; font-family:arial"><color="000000"><br/>' + 
-			'Vaším úkolem bude zhodnotit vzhled obrazců, které pro Vás mohou mít abstraktní povahu. Stiskněte klávesu <B>rightKey</B> pokud targetCat je více vizuálně rightAttribute než průměrný targetCat. ' + 
-			'Hit the <b>leftKey</b> key if it is more leftAttribute than average.<br/><br/>' + 
+			'Vaším úkolem bude zhodnotit vzhled obrazců, které pro Vás mohou mít abstraktní povahu (např. čínské piktogramy). Stiskněte klávesu <B>rightKey</B> pokud targetCat je vizuálně rightAttribute než průměrný targetCat. ' + 
+			'Stiskněte klávesu <b>leftKey</b> pokud je vizuálně leftAttribute než průměrný.<br/><br/>' + 
 			'Jednotlivé položky se velmi rychle zobrazí a zmizí.  ' + 
 			'Je důležité zdůraznit, že fotografie reálných objektů (např. obličejů) mohou někdy ovlivnit úsudek lidí při hodnocení abstraktních obrazců. Proto nás zajímá do jaké míry jsou lidé schopni vyhnout se těmto vlivům při hodnocení abstraktních obrazců. Zkuste proto prosím vyvinout co nejvyšší úsilí k tomu, abyste se vyhnuli ovlivnění fotografiemi reálných objektů. Dejte nám prosím Vaše upřímné hodnocení abstraktních obrazců bez ohledu na fotografie, které jim předchází.<br/><br/></p>'  + 
 			'<p style="font-size:16px; text-align:center; font-family:arial"><color="000000"><br/><br/>' + 
 			'Až na to budete připraveni, stisknutím tlačítka <b>space bar</b> spustíte pár cvičných pokusů.</p>' + 
 			'<p style="font-size:12px; text-align:center; font-family:arial">' + 
-			'<color="000000">[Round 1 of nBlocks]</p></div>',
+			'<color="000000">[1. kolo ze nBlocks]</p></div>',
 			firstBlockInst : '<div><p style="font-size:20px; text-align:left; margin-left:10px; font-family:arial"><color="000000"><br/>' + 
 			"Jde to rychle, co? Nevadí, pokud si některé nestihnete detailně prohlédnout" + 
 			'Řiďte se Vaším vnitřním pocitem.<br/><br/>' + 
-			'Soustřeďte se na každý targetCat a hodnoťe, jestli je více rightAttribute než dle Vašeho názoru průměrný targetCat stisknutím klávesy <b>rightKey</b>, ' + 
-			'nebo více leftAttribute než průměr stisknutím klávesy <b>leftKey</b>.<br/><br/>' + 
+			'Soustřeďte se na každý targetCat a hodnoťe, jestli je vizuálně rightAttribute než dle Vašeho názoru průměrný targetCat stisknutím klávesy <b>rightKey</b>, ' + 
+			'nebo vizuálně leftAttribute než průměr stisknutím klávesy <b>leftKey</b>.<br/><br/>' + 
 			'Hodnoťte pouze targetCat a ne položky, které jim předcházejí. ' + 
 			'Někdy Vám budou moci připadat rozptylující. To je v pořádku.<br/><br/>' + 
 			'<p style="font-size:16px; text-align:center; font-family:arial"><color="000000"><br/><br/>' + 
 			'Připraveni? Stiskněte <b>space bar</b>.</p>' + 
 			'<p style="font-size:12px; text-align:center; font-family:arial">' + 
-			'<color="000000">[Round 2 of nBlocks]</p></div>',
+			'<color="000000">[2. kolo ze nBlocks]</p></div>',
 			middleBlockInst : '<div><p style="font-size:20px; text-align:left; margin-left:10px; font-family:arial"><color="000000"><br/>' + 
 			'Pokračujte do dalšího kola úkolu. ' + 
 			'Pravidla jsou stále stejná:<br/><br/>' + 
-			'Soustřeďte se na každý targetCat a hodnoťe, jestli je více rightAttribute než dle Vašeho názoru průměrný targetCat stisknutím klávesy <b>rightKey</b>, ' + 
-			'nebo více leftAttribute než průměr stisknutím klávesy <b>leftKey</b>.<br/><br/>' + 
+			'Soustřeďte se na každý targetCat a hodnoťe, jestli je vizuálně rightAttribute než dle Vašeho názoru průměrný targetCat stisknutím klávesy <b>rightKey</b>, ' + 
+			'nebo vizuálně leftAttribute než průměr stisknutím klávesy <b>leftKey</b>.<br/><br/>' + 
 			'Hodnoťte pouze targetCat a ne položky, které jim předcházejí. ' + 
 			'Tyto položky Vás mohou rozptylovat. Řiďte se však při hodnocení Vaším vnitřním pocitem.<br/><br/>' + 
 			'<p style="font-size:16px; text-align:center; font-family:arial"><color="000000"><br/><br/>' + 
 			'Připraveni? Stiskněte <b>space bar</b>.</p>' + 
 			'<p style="font-size:12px; text-align:center; font-family:arial">' + 
-			'<color="000000">[Round blockNum of nBlocks]</p></div>',
+			'<color="000000">[3. kolo ze nBlocks]</p></div>',
 			lastBlockInst : '<div><p style="font-size:20px; text-align:left; margin-left:10px; font-family:arial"><color="000000"><br/>' + 
 			'Připraveni na FINÁLNÍ kolo? ' + 
 			'Pravidla sou pořád a stále stejná:<br/><br/>' + 
-			'Soustřeďte se na každý targetCat a hodnoťe, jestli je více rightAttribute než dle Vašeho názoru průměrný targetCat stisknutím klávesy <b>rightKey</b>, ' + 
-			'nebo více leftAttribute než průměr stisknutím klávesy <b>leftKey</b>.<br/><br/>' + 
+			'Soustřeďte se na každý targetCat a hodnoťe, jestli je vizuálně rightAttribute než dle Vašeho názoru průměrný targetCat stisknutím klávesy <b>rightKey</b>, ' + 
+			'nebo vizuálně leftAttribute než průměr stisknutím klávesy <b>leftKey</b>.<br/><br/>' + 
 			'Hodnoťte pouze targetCat a ne položky, které jim předcházejí. ' + 
 			'Tyto položky Vás mohou rozptylovat. Řiďte se však při hodnocení Vaším vnitřním pocitem.<br/><br/>' + 
 			'<p style="font-size:16px; text-align:center; font-family:arial"><color="000000"><br/><br/>' + 
 			'Připraveni? Stiskněte <b>space bar</b>.</p>' + 
 			'<p style="font-size:12px; text-align:center; font-family:arial">' + 
-			'<color="000000">[Round blockNum of nBlocks]</p></div>',
-		 targetCat : 'čínský symbol', //The name of the targets (used in the instructions)
+			'<color="000000">[4. kolo ze nBlocks]</p></div>',
+		 targetCat : 'čínský piktogram', //The name of the targets (used in the instructions)
 	});
 });
