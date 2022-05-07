ba2a88e8fdc6ddfe3641d85f84c02520e052a914
BrattCZE <104774374+BrattCZE@users.noreply.github.com>
2022-05-07 09:57:40 +0200

    Update RaceAMP02.js

--- RaceAMP02.js
+++ RaceAMP02.js
@@ -22,7 +22,14 @@ define(['pipAPI', 'https://brattcze.github.io/MainAMPScript.js'], function (APIC
                     {image: 'b09.jpg'},
                     {image: 'b10.jpg'},
                     {image: 'b11.jpg'},
-                    {image: 'b12.jpg'}]
+                    {image: 'b12.jpg'},
+                    {image: 'b13.jpg'},
+                    {image: 'b14.jpg'},
+                    {image: 'b15.jpg'},
+                    {image: 'b16.jpg'},
+                    {image: 'b17.jpg'},
+                    {image: 'b18.jpg'},
+                    {image: 'b19.jpg'}]
 
             },
             {
@@ -41,7 +48,14 @@ define(['pipAPI', 'https://brattcze.github.io/MainAMPScript.js'], function (APIC
                     {image: 'w09.jpg'},
                     {image: 'w10.jpg'},
                     {image: 'w11.jpg'},
-                    {image: 'w12.jpg'}]
+                    {image: 'w12.jpg'},
+                    {image: 'w13.jpg'},
+                    {image: 'w14.jpg'},
+                    {image: 'w15.jpg'},
+                    {image: 'w16.jpg'},
+                    {image: 'w17.jpg'},
+                    {image: 'w18.jpg'},
+                    {image: 'w19.jpg'}]
             }
         ],
         targetCats :  [
